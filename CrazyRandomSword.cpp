195080717ed7a2a1224de7e310d1b886380e7884
imd15 <imd15@my.fsu.edu>
2019-01-30 14:36:10 -0500

    created cpp file and h file for CrazyRandomSword. implemented random number gen to calculate damage inflicted.

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   CrazyRandomSword.cpp
+ * Author: Ian <imd15@my.fsu.edu>
+ *  
+ */
+
+#include <stdlib.h>
+#include "CrazyRandomSword.h"
+
+
+double CrazyRandomSword::hit(double armor){
+	int numOfArmorIgnored = rand()%(armor/3 -1)+2
+	
+	double damage = hitPoints - numOfArmorIgnored
+    
+	if(damage < 0){
+        return 0;
+    }
+    return damage;
+}
---
+++ CrazyRandomSword.h
@@ -0,0 +1,37 @@
+/* 
+ * File:   CrazyRandomSword.h
+ * Author: Ian <imd15@my.fsu.edu>
+ *
+ * Created on Jan 29, 2018
+ */
+
+#include <string>
+#include "Weapon.h"
+#include <cstdlib>
+#include <ctime>
+
+#ifndef CRAZYRANDOMSWORD_H
+#define CRAZYRANDOMSWORD_H
+
+/**
+ * Defines the behavior of a Simple Hammer (hitpoint = 25, ignores if less than 30 
+ * armor points)
+ */
+class CrazyRandomSword : public Weapon {
+public:
+	//the 10 hitpoints is arbitrary as it will be changed when the random number generatr is created
+    CrazyRandomSword() : Weapon("Crazy Random Sword", 10.0) {
+	srand(time(NULL)); //sets random seed
+	hitPoints = rand() 94 +7; //7-94
+
+
+	}
+
+    virtual ~CrazyRandomSword() {};
+
+    virtual double hit(double armor);
+
+};
+
+#endif /* CrazyRandomSword_H */
+
